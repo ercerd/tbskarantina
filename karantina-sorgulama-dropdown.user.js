d8d6df6200c34c32f6baea264de6cc6f9531d841
ercerd <ercerd@gmail.com>
2025-01-09 12:10:42 +0000

    Update karantina-sorgulama-dropdown.user.js

--- karantina-sorgulama-dropdown.user.js
+++ karantina-sorgulama-dropdown.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Karantina Sorgulama Dropdown Liste Güncellenen
 // @namespace   violentmonkey
-// @version     2.14
+// @version     2.15
 // @description Karantina BSS Uygunluk Sorgulama Sayfasına Sık Kullanılan Kapıların Sayı Başlangıçlarını Ekleme
 // @match       https://tbsapp.tarbil.gov.tr/Reports/ReportViewDynamic.aspx?report=*
 // @grant       none
